2eb8681a21abcebc710d6a42dd7cde1f690432a7
Daehyun Kyung <cokolavel@gmail.com>
2023-02-04 15:48:18 +0900

    basic print,input

---
+++ basic/inout.py
@@ -0,0 +1,49 @@
+c=input()
+print(c)
+
+c=input()
+c=int(c)
+print(c)
+
+f=input()
+f=float(f)
+print(f)
+
+a = input() 
+b = input()
+a=int(a)
+b=int(b)
+print(a)
+print(b)
+
+a = input() 
+b = input()
+print(b)
+print(a)
+
+f=input()
+f=float(f)
+print(f)
+print(f)
+print(f)
+
+a,b=input().split()
+a=int(a)
+b=int(b)
+print(a)
+print(b)
+
+a,b=input().split()
+print(b,a)
+
+s=input()
+print(s,s,s)
+
+a,b=input().split(':')
+print(a,b,sep=':')
+
+y,m,d=input().split('.')
+print(d,m,y,sep='-')
+
+bt,re=input().split('-')
+print(bt,re,sep='')
